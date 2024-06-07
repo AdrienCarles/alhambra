ff9d4011ff129c8f54f9812d721ff885303d03e3
AdrienCarles <adrien.carles@limayrac.fr>
2024-06-07 13:21:15 +0200

    Création d'user AC

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,28 +1,32 @@
 <?php
-// src/Controller/UserController.php
 namespace App\Controller;
+
 use App\Entity\User;
 use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 class UserController extends AbstractController
 {
-    /**
-     * @Route("/user/new", name="user_new", methods={"GET","POST"})
-     */
-    public function new(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function new(Request $request, UserPasswordHasherInterface $passwordEncoder): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $user->setPassword($passwordEncoder->encodePassword($user, $user->getPassword()));
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
+            $user->setPassword($passwordEncoder->hashPassword($user, $user->getPassword()));
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
             return $this->redirectToRoute('user_success');
         }
         return $this->render('user/new.html.twig', [
@@ -31,9 +35,6 @@ class UserController extends AbstractController
         ]);
     }
 
-    /**
-    * @Route("/user/success", name="user_success", methods={"GET"})
-    */
     public function success(): Response
     {
         return $this->render('user/success.html.twig');
