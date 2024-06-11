457a5301c67836a12507c6a875743bd64b44edc1
Julien Blanchon <julien.blanchon@limayrac.fr>
2024-06-11 14:22:02 +0200

    Navbar + bootstrap

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,8 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/app.scss';
+
+import {Alert,Button,Carousel,Collapse,Dropdown,Modal,Offcanvas,Popover,ScrollSpy,Tab,Toast,Tooltip} from 'bootstrap';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    plugins: {
+        // include whatever plugins you want
+        // but make sure you install these via npm!
+
+        // add browserslist config to package.json (see below)
+        autoprefixer: {}
+    }
+}
