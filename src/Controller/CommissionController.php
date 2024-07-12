28492f1a11e6e339024a5fc065e49e5e0aeb4dfc
Julien Blanchon <julien.blanchon@limayrac.fr>
2024-07-12 09:37:49 +0200

    commission css

--- src/Controller/CommissionController.php
+++ src/Controller/CommissionController.php
@@ -26,6 +26,7 @@ class CommissionController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $this->entityManager->persist($commission);
             $this->entityManager->flush();
+            $this->addFlash('success', 'Le groupe a été créé avec succès !'); 
             return $this->redirectToRoute('commission_success');
         }
         return $this->render('commission/new.html.twig', [
@@ -41,6 +42,16 @@ class CommissionController extends AbstractController
         return $this->render('commission/list.html.twig', [
             'groups' => $group,
         ]);
+    }
+
+    #[Route('/{id}/delete', name: 'commission_delete', methods: ['POST'])]
+    public function delete(Commission $commission, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($commission);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Le groupe a été supprimé avec succès.');
+        return $this->redirectToRoute('commission_list'); // Rediriger vers la liste des groupes
     }
 
 
